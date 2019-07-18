e8c60d41ae27b349d5c3eb1ae6702cd3bffed634
Allen Wang <allenaiir@gmail.com>
2019-07-19 00:01:45 +0800

    0719

---
+++ leetcode/n21-30/generate-parentheses.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        
+print(Solution().generateParenthesis(3))
