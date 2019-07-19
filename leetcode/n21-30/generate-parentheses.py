a96bcb867f72ad3e8ec5f37256e0cbff52be035f
allenair <allenair@gmail.com>
2019-07-19 09:00:11 +0800

    0719

--- leetcode/n21-30/generate-parentheses.py
+++ leetcode/n21-30/generate-parentheses.py
@@ -5,6 +5,17 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        finLst = []
+        self.DFS(n, n, '', finLst)
+        return finLst
+
+    def DFS(self, left, right, s, finLst):
+        if left == 0 and right == 0:
+            finLst.append(s)
+        if left > 0:
+            self.DFS(left - 1, right, s + '(', finLst)
+        if right > left:
+            self.DFS(left, right - 1, s + ')', finLst)
 
 
-print(Solution().generateParenthesis(3))
+print(Solution().generateParenthesis(2))
