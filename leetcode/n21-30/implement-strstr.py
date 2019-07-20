f60f467397d671b39a935b3351511b87e2002670
Allen Wang <allenaiir@gmail.com>
2019-07-21 00:03:55 +0800

    0720

--- leetcode/n21-30/implement-strstr.py
+++ leetcode/n21-30/implement-strstr.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 class Solution(object):
     def strStr(self, haystack, needle):
         """
@@ -7,14 +8,15 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        if not needle:
-            return 0 
-        
+        if not needle or haystack == needle:
+            return 0
+
         index = -1
-        for i in range(len(haystack)-len(needle)):
-            if haystack[i:i+len(needle)] == needle:
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
                 return i
 
         return index
 
-print(Solution().strStr("hello", 'lll'))
+
+print(Solution().strStr("mississippi", 'pi'))
