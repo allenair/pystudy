b8bc6256bf6d382267303fda2557e7216900ee24
allenair <allenair@gmail.com>
2019-07-23 11:58:23 +0800

    0723

--- leetcode/n31-40/next-permutation.py
+++ leetcode/n31-40/next-permutation.py
@@ -11,17 +11,21 @@ class Solution(object):
             return nums
 
         for i in range(len(nums) - 1, 0, -1):
-            for k in range(i-1, -1, -1):
-                if nums[i] > nums[k]:
-                    nums[i], nums[k] = nums[k], nums[i]
-                    for n in range(k+1, len(nums)-1):
-                        for m in range(n+1, len(nums)):
-                            nums[n], nums[m] = nums[m], nums[n]
-                    return nums
+            if nums[i] > nums[i - 1]:
+                for k in range(len(nums)-1, i-1, -1):
+                    if nums[k] > nums[i - 1]:
+                        nums[i - 1], nums[k] = nums[k], nums[i - 1]
+                        break
+                for n in range(i, len(nums) - 1):
+                    for m in range(n + 1, len(nums)):
+                        nums[n], nums[m] = nums[m], nums[n]
+                return nums
         else:
             nums.reverse()
 
         return nums
 
 
+print(Solution().nextPermutation([1,3,2]))
+print(Solution().nextPermutation([1,2,3]))
 print(Solution().nextPermutation([4,2,0,2,3,2,0]))
