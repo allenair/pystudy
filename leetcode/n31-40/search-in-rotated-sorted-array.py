65eb1c895081c6e84d2e567541c79bb7bdf262e5
allenair <allenair@gmail.com>
2019-07-25 17:44:41 +0800

    0725

---
+++ leetcode/n31-40/search-in-rotated-sorted-array.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        pass
+
+print(Solution().search([4,5,6,7,0,1,2],0))
