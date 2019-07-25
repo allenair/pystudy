643d0433c9ae37180e1a2f570e8fba6c3e55ed38
allenair <allenair@gmail.com>
2019-07-25 16:39:48 +0800

    0725

--- tfstudy/mytest.py
+++ tfstudy/mytest.py
@@ -1,19 +1,24 @@
-mylist = [7,2,3,4,5,6]
+import math
+import mydemo
 
-mylist.sort()
+sv1 = [1, 1, 2, 1, 1, 1, 0, 0, 0]
+sv2 = [1, 1, 1, 0, 1, 1, 1, 1, 1]
 
-print(mylist)
+dotVal = sum([sv1[i] * sv2[i] for i in range(len(sv1))])
+rank1 = math.sqrt(sum([sv1[i]**2 for i in range(len(sv1))]))
+rank2 = math.sqrt(sum([sv2[i]**2 for i in range(len(sv2))]))
 
-myset=set()
-myset.add((1,2))
-myset.add((1,3))
-myset.add((1,2))
+print(dotVal / (rank1 * rank2))
 
-print(myset)
+word_dict = {'a': [0.1, 0.2, 0.3], 'b': [0.5, 0.7, 0.1], 'c': [0.2, 0.4, 0.6]}
+res = []
+for _, vector in word_dict.items():
+    if not res:
+        res = [0.0] * len(vector)
+    res = [res[i] + vector[i] for i in range(len(res))]
 
-mytup = tuple(mylist)
-
-print(mytup)
-
-mylist = [list(x) for x in myset]
-print(mylist)
+print(res)
+tmp = [res[i]**2 for i in range(len(res))]
+total = sum(tmp)
+total = math.sqrt(total)
+print([res[i] / total for i in range(len(res))])
