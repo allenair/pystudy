c3cf0abf8a45a80ef4c4c9a0a91ec4815fb702c0
Allen Wang <allenaiir@gmail.com>
2019-08-25 00:18:23 +0800

    0824

--- leetcode/n31-40/sudoku-solver.py
+++ leetcode/n31-40/sudoku-solver.py
@@ -1,37 +1,54 @@
 # -*- coding: utf-8 -*-
 
+
 class Solution(object):
     def solveSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: None Do not return anything, modify board in-place instead.
         """
-        pass
+        row = [[0] * 10 for i in range(9)]
+        col = [[0] * 10 for i in range(9)]
+        box = [[0] * 10 for i in range(9)]
 
-    def isValidSudoku(self, board):
-        for arr in board:
-            if not self.is_right(arr):
-                return False
-        
         for i in range(9):
-            arr = [arr[i] for arr in board]
-            if not self.is_right(arr):
-                return False
+            for k in range(9):
+                if board[i][k] != '.':
+                    num = int(board[i][k])
+                    row[i][num] = 1
+                    col[k][num] = 1
+                    box[i // 3 * 3 + k // 3][num] = 1
+
+        self.solve(board, 0, 0, row, col, box)
+
+    def solve(self, board, x, y, row, col, box):
+        if x==9:
+            return True
         
-        for k in range(0,9,3):
-            for n in range(0,9,3):
-                arr = [board[i][j] for i in range(k,k+3) for j in range(n,n+3)]
-                if not self.is_right(arr):
-                    return False
+        ny = (y+1) % 9
+        nx = x+1 if ny==0 else x
 
-        return True
-    
-    def is_right(self, nums):
-        lst = []
-        for num in nums:
-            if num in lst:
-                return False
-            if num !='.':
-                lst.append(num)
-        return True
+        if board[x][y]!='.':
+            return self.solve(board, nx, ny, row, col, box)
 
+        box_index = x//3*3+y//3
+        for i in range(1,10):
+            if (not row[x][i]) and (not col[y][i]) and (not box[box_index][i]):
+                board[x][y]=str(i)
+                row[x][i]=1
+                col[y][i]=1
+                box[box_index][i]=1
+                if self.solve(board, nx, ny, row, col, box):
+                    return True
+                board[x][y]='.'
+                row[x][i]=0
+                col[y][i]=0
+                box[box_index][i]=0
+        
+        return False
+
+board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+Solution().solveSudoku(board)
+print(board)
+
+   
